2f2a91342c85e87357ab978308a3750404d3820f
kaviyakris-lang <kaviyakris@gmail.com>
2025-12-14 12:18:02 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -5,6 +5,6 @@ public class Helloworld{
     */
 
    public static void main(String []args) {
-      System.out.println("Hello World"); // prints Hello World
+      System.out.println("Hello World to the world"); // prints Hello World
    }
 }
